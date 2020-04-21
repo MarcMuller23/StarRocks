121a7483277fe3567b0233f2153509cd8215d8ac
Joep Sales <sales.joep@gmail.com>
2020-04-21 12:06:11 +0200

    Added Event Entity

---
+++ StarRocks/StarRocks.Data/Entities/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarRocks.Data.Entities
+{
+    class Event
+    {
+        public int ID { get; set; }
+        public int AccountID { get; set; }
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public int Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int MaxCapacity { get; set; }
+    }
+}
