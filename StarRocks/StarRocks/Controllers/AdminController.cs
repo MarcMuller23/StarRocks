db61a6419d118db1bd6137759be238142d227e40
StarwarsShooter <61284683+StarwarsShooter@users.noreply.github.com>
2020-04-22 19:58:50 +0200

    Toevoeging: Admin map met AdminEvents, AdminFav, AdminInbox, AdminMaps, AdminProfile, AdminSent, AdminTrash Toevoeging in Home map: Login, Register Veranderd: _Layout, site.css, bootstrap.css

---
+++ StarRocks/StarRocks/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RockStarsInlog.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult AdminProfile()
+        {
+            return View();
+        }
+
+        public IActionResult AdminInbox()
+        {
+            return View();
+        }
+
+        public IActionResult AdminEvents()
+        {
+            return View();
+        }
+
+        public IActionResult AdminFav()
+        {
+            return View();
+        }
+
+        public IActionResult AdminMaps()
+        {
+            return View();
+        }
+
+        public IActionResult AdminSent()
+        {
+            return View();
+        }
+
+        public IActionResult AdminTrash()
+        {
+            return View();
+        }
+    }
+}
