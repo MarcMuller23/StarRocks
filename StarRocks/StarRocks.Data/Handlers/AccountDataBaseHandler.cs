bb2ace76bf9f87f57de2286f3e70c60515bac384
MarcMuller23 <marc_muller@hotmail.com>
2020-05-07 10:37:50 +0200

    Full Handler list

--- StarRocks/StarRocks.Data/Handlers/AccountDataBaseHandler.cs
+++ StarRocks/StarRocks.Data/Handlers/AccountDataBaseHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using StarRocks.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,106 @@ namespace StarRocks.Data.Handlers
 {
     class AccountDataBaseHandler
     {
+        private static string connectionString = "";
+
+        public static void SetConnectionString(string constring)
+        {
+            connectionString = constring;
+        }
+       
+        public List<Account> GetAllAccounts()
+        {
+            List<Account> Accounts = new List<Account>();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM account";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    conn.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Account dto = new Account();
+                        dto.ID = reader.GetInt32(0);
+                        dto.FirstName = reader.GetString(1);
+                        dto.Preposition = reader.GetString(2);
+                        dto.LastName = reader.GetString(3);
+                        dto.Email = reader.GetString(4);
+                        dto.Password = reader.GetString(5);
+                        dto.PhoneNumber = reader.GetString(6);
+                        dto.Street = reader.GetString(7);
+                        dto.HouseNumber = reader.GetInt32(8);
+                        dto.Addition = reader.GetString(9);
+                        dto.PostalCode = reader.GetString(10);
+                        dto.City = reader.GetString(11);
+                        dto.Birthdate = reader.GetDateTime(12);
+                        Accounts.Add(dto);
+                    }
+                }
+            }
+            return Accounts;
+        }
+        public void CreateAccount(Account A1)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Account VALUES( @FirstName,@Preposition,@LastName,@Email,@Password,@PhoneNumber,@Street,@HouseNumber,@Addition,@PostalCode,@City,@Birthdate; ";
+
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {                    
+                    command.Parameters.AddWithValue("@FirstName", A1.FirstName);
+                    command.Parameters.AddWithValue("@Preposition", A1.Preposition);
+                    command.Parameters.AddWithValue("@LastName", A1.LastName);
+                    command.Parameters.AddWithValue("@Email", A1.Email);
+                    command.Parameters.AddWithValue("@Password", A1.Password);
+                    command.Parameters.AddWithValue("@PhoneNumber", A1.PhoneNumber);
+                    command.Parameters.AddWithValue("@Street", A1.Street);
+                    command.Parameters.AddWithValue("@Addition", A1.Addition);
+                    command.Parameters.AddWithValue("@PostalCode", A1.PostalCode);
+                    command.Parameters.AddWithValue("@City", A1.City);
+                    command.Parameters.AddWithValue("@Birthdate", A1.Birthdate);
+                    
+
+
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void UpdateAccount(Account A1)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "UPDATE account SET Firstname = @FirstName, Preposition = @Preposition, LastName=@LastName,Email=@Email,Password=@Password,PhoneNumber=@PhoneNumber,Street=@Street,Addition=@Addition,PostalCode=@PostalCode,City=@City,Birthdate=@Birthdate WHERE ID=@ID; ";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@ID", A1.ID);
+                    command.Parameters.AddWithValue("@FirstName", A1.FirstName);
+                    command.Parameters.AddWithValue("@Preposition", A1.Preposition);
+                    command.Parameters.AddWithValue("@LastName", A1.LastName);
+                    command.Parameters.AddWithValue("@Email", A1.Email);
+                    command.Parameters.AddWithValue("@Password", A1.Password);
+                    command.Parameters.AddWithValue("@PhoneNumber", A1.PhoneNumber);
+                    command.Parameters.AddWithValue("@Street", A1.Street);
+                    command.Parameters.AddWithValue("@Addition", A1.Addition);
+                    command.Parameters.AddWithValue("@PostalCode", A1.PostalCode);
+                    command.Parameters.AddWithValue("@City", A1.City);
+                    command.Parameters.AddWithValue("@Birthdate", A1.Birthdate);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void DeleteAccount(int ID)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "DELETE FROM account WHERE ID=@ID";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@ID", ID);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
--- StarRocks/StarRocks.Data/Handlers/CatagoryDataBaseHandler.cs
+++ StarRocks/StarRocks.Data/Handlers/CatagoryDataBaseHandler.cs
@@ -1,10 +1,83 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using StarRocks.Data.Entities;
 using System.Collections.Generic;
-using System.Text;
 
 namespace StarRocks.Data.Handlers
 {
     class CatagoryDataBaseHandler
     {
+        private static string connectionString = "";
+
+        public static void SetConnectionString(string constring)
+        {
+            connectionString = constring;
+        }
+
+        public List<Categorie> GetAllCatagory()
+        {
+            List<Categorie> Categories = new List<Categorie>();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Categorie";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    conn.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Categorie dto = new Categorie();
+                        dto.ID = reader.GetInt32(0);
+                        dto.AccountID = reader.GetInt32(1);
+                        dto.Name = reader.GetString(2);
+                        dto.Description = reader.GetString(3);
+                        Categories.Add(dto);
+                    }
+                }
+            }
+            return Categories;
+        }
+        public void CreateEvent(Categorie C1)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Categorie VALUES(@AccountID, @Name,@Description ";
+
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@AccountID", C1.AccountID);
+                    command.Parameters.AddWithValue("@Name", C1.Name);
+                    command.Parameters.AddWithValue("@Description", C1.Description);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void UpdateEvent(Categorie C1)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = $"UPDATE Categorie SET AccountID = @AccountID, Name = @Name, Description=@Description WHERE ID=@ID; ";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@ID", C1.ID);
+                    command.Parameters.AddWithValue("@AccountID", C1.AccountID);
+                    command.Parameters.AddWithValue("@Name", C1.Name);
+                    command.Parameters.AddWithValue("@Description", C1.Description);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void DeleteEvent(int ID)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = $"DELETE FROM Categorie WHERE ID=@ID";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@ID", ID);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
+
