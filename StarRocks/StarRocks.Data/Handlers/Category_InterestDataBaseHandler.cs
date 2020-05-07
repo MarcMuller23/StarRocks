35414766fcbd3ccf87059678e2ea3e71a875ee6f
MarcMuller23 <marc_muller@hotmail.com>
2020-05-07 10:37:50 +0200

    Full Handler list

--- StarRocks/StarRocks.Data/Handlers/Category_InterestDataBaseHandler.cs
+++ StarRocks/StarRocks.Data/Handlers/Category_InterestDataBaseHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using StarRocks.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,80 @@ namespace StarRocks.Data.Handlers
 {
     class Category_InterestDataBaseHandler
     {
+        private static string connectionString = "";
+
+        public static void SetConnectionString(string constring)
+        {
+            connectionString = constring;
+        }
+
+        public List<Categorie_Interest> GetAllCategorie_Interest()
+        {
+            List<Categorie_Interest> Categorie_Interests = new List<Categorie_Interest>();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Categorie_Interests";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    conn.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Categorie_Interest dto = new Categorie_Interest();
+                        dto.ID = reader.GetInt32(0);
+                        dto.CategorieID = reader.GetInt32(1);
+                        dto.AccountID = reader.GetInt32(2);
+                        dto.EventPoint = reader.GetInt32(3);
+
+                        Categorie_Interests.Add(dto);
+                    }
+                }
+            }
+            return Categorie_Interests;
+        }
+        public void CreateCategorie_Interest(Categorie_Interest CI1)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Categorie_Interest VALUES( @CategorieID,@AccountID,@EventPoint,@Password,@PhoneNumber,@Street,@HouseNumber,@Addition,@PostalCode,@City,@Birthdate; ";
+
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    
+                    command.Parameters.AddWithValue("@CategorieID", CI1.CategorieID);
+                    command.Parameters.AddWithValue("@AccountID", CI1.AccountID);
+                    command.Parameters.AddWithValue("@EventPoint", CI1.EventPoint);   
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void UpdateCategorie_Interest(Categorie_Interest CI1)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "UPDATE Categorie_Interest SET CategorieID = @CategorieID, AccountID = @AccountID, EventPoint=@EventPoint WHERE ID=@ID; ";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@ID", CI1.ID);
+                    command.Parameters.AddWithValue("@CategorieID", CI1.CategorieID);
+                    command.Parameters.AddWithValue("@AccountID", CI1.AccountID);
+                    command.Parameters.AddWithValue("@EventPoint", CI1.EventPoint);                    
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void DeleteCategorie_Interest(int ID)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Categorie_Interest WHERE ID=@ID";
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@ID", ID);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
+
