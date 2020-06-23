ee86941939cce9179e8766e68890f89316af5428
Kursat <kursat38tr@hotmail.com>
2020-06-23 13:12:26 +0200

    Joepie

---
+++ StarRocks/StarRocks-master/StarRocks/StarRocks.Interfaces/Entities/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace StarRocks.Interfaces.Entities
+{
+    public interface IUser
+    {
+        string Firstname { get; set; }
+        string Preposition { get; set; }
+        string Lastname { get; set; }
+        string Street { get; set; }
+        int Housenumber { get; set; }
+        string Addition { get; set; }
+        string Postcalcode { get; set; }
+        string City { get; set; }
+        DateTime Birthday { get; set; }
+    }
+}
